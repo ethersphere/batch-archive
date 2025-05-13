cb06f2a21399789b7253507d6a54b11a97ef4457
Ljubisa Gacevic <ljubisa.rs@gmail.com>
2025-05-13 14:10:23 +0200

    fix: update and leave only compressed snapshot

--- batcharchive.go
+++ batcharchive.go
@@ -4,15 +4,9 @@ import (
 	_ "embed"
 )
 
-//go:embed archive/export.ndjson
-var payloadJSON []byte
-
 //go:embed archive/export.ndjson.gzip
 var payloadGZIP []byte
 
-func GetBatchSnapshot(compress bool) []byte {
-	if compress {
-		return payloadGZIP
-	}
-	return payloadJSON
+func GetBatchSnapshot() []byte {
+	return payloadGZIP
 }
